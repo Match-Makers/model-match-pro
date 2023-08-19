3e1439eb4632a9dc7fc2478571a2458f4fa09549
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-08-19 12:26:49 -0500

    feat: added styling to signin

--- frontend/components/SignUpForm.js
+++ frontend/components/SignUpForm.js
@@ -1,19 +1,21 @@
-
 export default function SignUpform() {
   return (
-    <form id='signup-form'>
-      <label htmlfor='username'> Username </label>
+    <form id="signup-form">
+      <label htmlFor="username"> Username </label>
       <input type="text" id="username" name="username" required></input>
 
-      <label htmlfor='password'> password </label>
+      <label htmlFor="password"> password </label>
       <input type="password" id="password" name="password" required></input>
 
-      <label htmlfor='confirm-password'> confirm password </label>
-      <input type="password" id="confirm-password'" name="confirm-password'" required></input>
+      <label htmlFor="confirm-password"> confirm password </label>
+      <input
+        type="password"
+        id="confirm-password'"
+        name="confirm-password'"
+        required
+      ></input>
 
-      <button>
-        Submit
-      </button>
+      <button>Submit</button>
     </form>
-  )
+  );
 }
