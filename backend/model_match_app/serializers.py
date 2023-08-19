fcaf1d6d317a35acb0ecaee44855cc45a3dd90b4
Manuch Sadri <colonquebang@hotmail.com>
2023-08-19 10:35:39 -0700

    updates models.py and views.py for APIViews

--- backend/model_match_app/serializers.py
+++ backend/model_match_app/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from .models import LLM, Prompt, Responses
 
+
 class LLMSerializer(serializers.ModelSerializer):
     class Meta:
         model = LLM
         fields = '__all__'
 
+
 class ResponsesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Responses
         fields = '__all__'
+
 
 class PromptSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/model_match_app/views.py
+++ backend/model_match_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .models import LLM, Prompt, Responses
+from .permissions import IsOwnerOrReadOnly
+from .serializers import LLMSerializer, PromptSerializer, ResponsesSerializer
 
-# Create your views here.
+
+class PromptList(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    serializer_class = PromptSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Prompt.objects.filter(user_id=user)
+
+
+class ResponseDetail(ListAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
+    serializer_class = ResponsesSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Responses.objects.filter(user_id=user)
+
+
+class LLMList(ListAPIView):
+    queryset = LLM.objects.all()
+    serializer_class = LLMSerializer
