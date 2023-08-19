c7b31ab46281344c7811ebe1fa49504e242e14b6
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-08-19 15:07:02 -0700

    search bar, responses

--- frontend/components/SearchBar.js
+++ frontend/components/SearchBar.js
@@ -1,14 +1,14 @@
 export default function SearchBar() {
   return (
     <div className="flex flex-col max-w-2xl max-h-screen mx-auto">
-      <div class="input-group">
+      <div className="input-group">
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="Enter your prompt"
         />
-        <div class="input-group-append">
-          <button class="btn btn-secondary" type="button">
+        <div className="input-group-append">
+          <button className="btn btn-secondary" type="button">
             Search
           </button>
         </div>
