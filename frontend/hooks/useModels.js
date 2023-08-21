67803c3ea291d2848ed0f3cfb9167b5b7c4be88c
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-08-21 07:57:14 -0500

    fix: corrected typo error

--- frontend/hooks/useModels.js
+++ frontend/hooks/useModels.js
@@ -3,34 +3,11 @@ import useSWR from 'swr';
 const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/model_match_app/`;
 
 import { useAuth } from '@/contexts/auth';
-import { useEffect, useState } from 'react';
-
-const MOCK_DATA = [
-  {
-    model: 'Bard',
-    code: 'bard_code',
-  },
-  {
-    model: 'GPT',
-    code: 'gpt_code',
-  },
-  {
-    model: 'JerryBot',
-    code: 'jerry_code',
-    id: 'jerry_code',
-  },
-  {
-    model: 'DeioshaBot',
-    code: 'deiosha_code',
-  },
-];
+import { useState } from 'react';
 
 export default function useModels() {
   const { tokens } = useAuth();
   const { data = [], error } = useSWR([apiUrl, tokens], fetchModels);
-  //   TEMP
-  console.warn('API Data:', data, 'url:', apiUrl);
-  //
   const [selectedModels, setSelectedModels] = useState([]);
 
   async function fetchModels() {
