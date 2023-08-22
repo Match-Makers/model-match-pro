d684a6e08dfc379cf2c6ed0fe1355f255b77ebf6
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-08-21 20:09:02 -0700

    setting up prompt hook

--- frontend/components/SearchBar.js
+++ frontend/components/SearchBar.js
@@ -1,8 +1,19 @@
+import useModels from '@/hooks/useModels';
+import usePrompts from '@/hooks/usePrompts';
+import { useState } from 'react';
+
 export default function SearchBar() {
+  const { selectedModels } = useModels();
+
+  const [text, setText] = useState('');
+  const {createPrompt} = usePrompts();
+
   return (
     <div className="flex flex-col max-w-2xl max-h-screen mx-auto my-4">
       <div className="input-group">
         <input
+          value={text}
+          onChange={ (val) => setText(val)}
           type="text"
           className="form-control"
           placeholder="Enter your prompt"
---
+++ frontend/hooks/usePrompts.js
@@ -0,0 +1,59 @@
+import useSWR from 'swr';
+
+const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/model_match_app/prompts/`;
+
+import { useAuth } from '@/contexts/auth';
+import { useState } from 'react';
+
+export default function useModels() {
+  const { tokens } = useAuth();
+  const { data = [], error } = useSWR([apiUrl, tokens], fetchPrompts);
+  const [prompt, setPrompt] = useState([]);
+
+  async function fetchPrompts() {
+    try {
+      if (!tokens) {
+        throw new Error('No auth tokens found');
+      }
+      const response = await fetch(apiUrl, config());
+      const responseJSON = await response.json();
+      return responseJSON;
+    } catch (err) {
+      handleError(err);
+    }
+  }
+
+  async function createPrompt(info) {
+    try {
+      const options = config();
+      (options.method = 'POST'), (options.body = JSON.stringify(info));
+      await fetch(apiUrl, options);
+      mutate(); // mutate causes complete collection to be refetched
+    } catch (err) {
+      handleError(err);
+    }
+  }
+
+  function handleError(err) {
+    console.error(`fetchPrompts: ${err}`);
+  }
+
+  function config() {
+    return {
+      headers: {
+        Authorization: 'Bearer ' + tokens.access,
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+
+
+  return {
+    models: data,
+    prompt,
+    error,
+    loading: tokens && !error && !data,
+    createPrompt,
+  };
+}
