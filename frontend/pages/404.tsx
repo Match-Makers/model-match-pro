c74ab5b44c6b09c3ae25dd02b4f80d26b4ae1ff8
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-09-12 20:10:37 -0700

    fix: 404

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,13 +1,23 @@
-import Image from 'next/image'
-import image404 from '../img/404.jpg'
+import Image from 'next/image';
+import image404 from '../img/404.jpg';
 
-export default function Custom404(){
-  return(
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#E0F7FA', paddingTop: '25px' }}>
-    <h1>Oops!</h1>
-    <h2>404 - Page not found</h2>
-    <p>Looks like you have ventured off the path...</p>
-    <Image src={image404} alt="404 image" width={700} height={700} />
-  </div>
-  ); 
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#E0F7FA',
+        paddingTop: '25px',
+      }}
+    >
+      <h1>Oops!</h1>
+      <h2>404 - Page not found</h2>
+      <p>Looks like you have ventured off the path...</p>
+      <Image src={image404} alt="404 image" width={700} height={700} />
+    </div>
+  );
 }
